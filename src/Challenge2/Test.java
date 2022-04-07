4136e02a0a6c6b6c73cc5979f1d20930535f24cc
EricM <101633226+EricMauler@users.noreply.github.com>
2022-04-07 10:57:17 +0200

    test

--- src/Challenge2/Test.java
+++ src/Challenge2/Test.java
@@ -1,12 +1,27 @@
 package Challenge2;
 
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class Test {
 
 	public static void main(String[] args) {
-		
-		// Test de sébastien !
-		//Test de Tristan!
-		
+
+		JFrame frame = new JFrame("Back to work !");
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+
+		JButton button = new JButton();
+		button.setText("OK !");
+
+		panel.add(button);
+		frame.add(panel);
+		frame.setSize(300, 400);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 
 }
